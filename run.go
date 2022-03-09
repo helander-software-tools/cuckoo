edbe592f2723e0364ad76e468dd0bb4ae7465762
helander <lehswe@gmail.com>
2022-03-09 23:25:46 +0100

    Update run.go

--- run.go
+++ run.go
@@ -56,7 +56,7 @@ func parent() {
 		//Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWIPC,
 	//}
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWPID,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
